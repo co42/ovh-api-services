9e3610fa885abd37efebb6e77b09fd39c5193aab
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2020-03-13 10:15:09 +0100

    refactor(pcc.datacenter.drp): apply prettier rules
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

--- src/api/dedicatedCloud/datacenter/disasterRecovery/zerto/dedicatedCloud-datacenter-disasterRecovery-zerto.v6.service.js
+++ src/api/dedicatedCloud/datacenter/disasterRecovery/zerto/dedicatedCloud-datacenter-disasterRecovery-zerto.v6.service.js
@@ -1,50 +1,61 @@
-angular.module('ovh-api-services').service('OvhApiDedicatedCloudDatacenterDisasterRecoveryZertoV6', ($resource, $cacheFactory) => {
-  const cache = $cacheFactory('OvhApiDedicatedCloudDatacenterDisasterRecoveryZertoV6');
+angular
+  .module('ovh-api-services')
+  .service(
+    'OvhApiDedicatedCloudDatacenterDisasterRecoveryZertoV6',
+    ($resource, $cacheFactory) => {
+      const cache = $cacheFactory(
+        'OvhApiDedicatedCloudDatacenterDisasterRecoveryZertoV6',
+      );
 
-  const interceptor = {
-    response(response) {
-      cache.remove(response.config.url);
-      return response.data;
+      const interceptor = {
+        response(response) {
+          cache.remove(response.config.url);
+          return response.data;
+        },
+      };
+
+      const baseUrl = '/dedicatedCloud/:serviceName/datacenter/:datacenterId/disasterRecovery/zerto';
+
+      const zertoResource = $resource(
+        baseUrl,
+        {
+          serviceName: '@serviceName',
+          datacenterId: '@datacenterId',
+        },
+        {
+          // Method is a POST but acts like a GET
+          get: {
+            url: `${baseUrl}/state`,
+            method: 'POST',
+            cache,
+            hasBody: false,
+          },
+          disable: {
+            url: `${baseUrl}/disable`,
+            method: 'POST',
+            interceptor,
+          },
+          enable: {
+            url: `${baseUrl}/enable`,
+            method: 'POST',
+            interceptor,
+          },
+          generateZsspPassword: {
+            url: `${baseUrl}/generateZsspPassword`,
+            method: 'POST',
+            interceptor,
+          },
+        },
+      );
+
+      zertoResource.resetAllCache = function resetAllCache() {
+        zertoResource.resetCache();
+      };
+
+      zertoResource.resetCache = function resetCache() {
+        cache.removeAll();
+      };
+
+      return zertoResource;
     },
-  };
-
-  const baseUrl = '/dedicatedCloud/:serviceName/datacenter/:datacenterId/disasterRecovery/zerto';
-
-  const zertoResource = $resource(baseUrl, {
-    serviceName: '@serviceName',
-    datacenterId: '@datacenterId',
-  }, {
-    // Method is a POST but acts like a GET
-    get: {
-      url: `${baseUrl}/state`,
-      method: 'POST',
-      cache,
-      hasBody: false,
-    },
-    disable: {
-      url: `${baseUrl}/disable`,
-      method: 'POST',
-      interceptor,
-    },
-    enable: {
-      url: `${baseUrl}/enable`,
-      method: 'POST',
-      interceptor,
-    },
-    generateZsspPassword: {
-      url: `${baseUrl}/generateZsspPassword`,
-      method: 'POST',
-      interceptor,
-    },
-  });
-
-  zertoResource.resetAllCache = function () {
-    zertoResource.resetCache();
-  };
-
-  zertoResource.resetCache = function () {
-    cache.removeAll();
-  };
-
-  return zertoResource;
-});
+  );
